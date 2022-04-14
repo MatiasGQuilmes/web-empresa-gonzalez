eb3a1d14e082bf058489263ca2d1f3d0c7461538
Matias Gonzalez <matyyassgonzalez89@gmail.com>
2022-04-14 20:58:43 -0300

    logo animado

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
+const {index, quienessomos} = require('../controllers/indexcontroller')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+/* router.get('/', function(req, res, next) {
+  res.render('index', { title: 'fidel-gonzalez' });
+}); */
+router.get('/', index)
+router.get('/quienessomos/', quienessomos)
 
 module.exports = router;
